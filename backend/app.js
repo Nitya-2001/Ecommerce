9e1a12802b152bc6430d1a9f82704c2fdcc6981e
surendra <surendra.singh_cs20@gla.ac.in>
2023-05-16 21:43:35 +0530

    mongodb connect

--- backend/app.js
+++ backend/app.js
@@ -1,7 +1,7 @@
 const express = require("express");
 
 const app = express();
-app.use(express.json);
+app.use(express.json());
 const product = require("./routes/productRoute");
 app.use("/api/v1/", product);
 
