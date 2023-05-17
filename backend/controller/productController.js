e9c89734ae8551dab99cedf71eadbadd9055fdcf
surendra <surendra.singh_cs20@gla.ac.in>
2023-05-17 16:42:24 +0530

    worked on Rest Api

--- backend/controller/productController.js
+++ backend/controller/productController.js
@@ -1,6 +1,6 @@
 const Product = require("../models/productModel");
 
-//Create Product
+//Create Product -- Admin
 
 exports.createProduct = async (req, res, next) => {
   const product = await Product.create(req.body);
@@ -11,8 +11,73 @@ exports.createProduct = async (req, res, next) => {
   });
 };
 
-exports.getAllProducts = (req, res) => {
+//Get all product
+
+exports.getAllProducts = async (req, res) => {
+  const products = await Product.find();
+
   res.status(200).json({
-    message: "Route is working fine",
+    success: true,
+    products,
+  });
+};
+
+//Get single eproduct details
+
+exports.getProductDetails = async (req, res, next) => {
+  const product = await Product.findById(req.params.id);
+  if (!product) {
+    return res.status(500).json({
+      success: false,
+      message: "Product not found",
+    });
+  }
+
+  res.status(200).json({
+    success: true,
+    product,
+  });
+};
+
+//Update Product -- Admin
+
+exports.updateProduct = async (req, res, next) => {
+  let product = Product.findById(req.params.id);
+
+  if (!product) {
+    return res.status(500).json({
+      success: false,
+      message: "Product not found",
+    });
+  }
+
+  product = await Product.findByIdAndUpdate(req.params.id, req.body, {
+    new: true,
+    runValidators: true,
+    useFindModify: false,
+  });
+
+  res.status(200).json({
+    success: true,
+    product,
+  });
+};
+
+// Delete Product
+
+exports.deleteProduct = async (req, res, next) => {
+  const product = await Product.findById(req.params.id);
+
+  if (!product) {
+    return res.status(500).json({
+      success: false,
+      message: "Product not found",
+    });
+  }
+  await product.deleteOne();
+
+  res.status(200).json({
+    success: true,
+    message: "Product Deleted Successfully",
   });
 };
